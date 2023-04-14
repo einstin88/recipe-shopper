69243d1d3b673250ff605d13de0987b0346bb88f
Pelie <pelie_888888@hotmail.com>
2023-04-14 17:36:02 +0800

    Commit setup files for initialization

---
+++ server/backend-server/recipe-shopper/src/test/java/com/main/backend/recipeshopper/RecipeShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.backend.recipeshopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
