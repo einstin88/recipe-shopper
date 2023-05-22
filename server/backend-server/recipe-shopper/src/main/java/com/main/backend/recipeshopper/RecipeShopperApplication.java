1f85133de33e5d175ae710f843f5d4badd5072cc
Pelie <pelie_888888@hotmail.com>
2023-05-22 23:22:44 +0800

    CI/CD with docker & k8s is working. finally

--- server/backend-server/recipe-shopper/src/main/java/com/main/backend/recipeshopper/RecipeShopperApplication.java
+++ server/backend-server/recipe-shopper/src/main/java/com/main/backend/recipeshopper/RecipeShopperApplication.java
@@ -2,15 +2,14 @@ package com.main.backend.recipeshopper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+// import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 // TODO Uncomment to enable scheduled task to run
-@EnableScheduling
+// @EnableScheduling
 public class RecipeShopperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeShopperApplication.class, args);
 	}
-
 }
